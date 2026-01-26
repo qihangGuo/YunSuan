9d6f837c341e13c5e87b08c9c29b44da63d9c52d
Xujinsheng <xujinsheng@xjs.net.cn>
2026-01-26 11:19:13 +0800

    refactor(VALUUtils, VAluBundles): use VlFuParameters and support to scale with VLEN

--- src/main/scala/yunsuan/vector/VectorALU/VIFuParameters.scala
+++ src/main/scala/yunsuan/vector/VectorALU/VIFuParameters.scala
@@ -1,11 +1,13 @@
 package yunsuan.vector
 
+import chisel3.util.log2Ceil
+
 object VIFuParam {
   val VLEN = 128
   val XLEN = 64
-  val VLENB = VLEN/8
-  val wVL = 8
-  val wVSTART = 7
+  val VLENB = VLEN / 8
+  val wVL = log2Ceil(VLEN + 1)
+  val wVSTART = log2Ceil(VLEN)
   val wUOPIDX = 6
   val wOPCODE = 6
 }
